b761da3ddc54326dd831d6d8b6d770bc3580241e
LibXZR <xzr467706992@163.com>
2020-08-10 20:03:11 +0800

    Merge tag 'v4.19.127' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into baseline
    This is the 4.19.127 stable release

--- security/integrity/evm/evm_main.c
+++ security/integrity/evm/evm_main.c
@@ -102,9 +102,8 @@ static int evm_find_protected_xattrs(struct dentry *dentry)
 	if (!(inode->i_opflags & IOP_XATTR))
 		return -EOPNOTSUPP;
 
-	for (xattr = evm_config_xattrnames; *xattr != NULL; xattr++) {
-		error = __vfs_getxattr(dentry, inode, *xattr, NULL, 0,
-				       XATTR_NOSECURITY);
+	list_for_each_entry_lockless(xattr, &evm_config_xattrnames, list) {
+		error = __vfs_getxattr(dentry, inode, xattr->name, NULL, 0);
 		if (error < 0) {
 			if (error == -ENODATA)
 				continue;
@@ -234,7 +233,7 @@ static int evm_protected_xattr(const char *req_xattr_name)
 	struct xattr_list *xattr;
 
 	namelen = strlen(req_xattr_name);
-	list_for_each_entry_rcu(xattr, &evm_config_xattrnames, list) {
+	list_for_each_entry_lockless(xattr, &evm_config_xattrnames, list) {
 		if ((strlen(xattr->name) == namelen)
 		    && (strncmp(req_xattr_name, xattr->name, namelen) == 0)) {
 			found = 1;
